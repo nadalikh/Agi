5b911378ef831d44153b526ffb55e455857c5ca1
Nadali Khalili <nadalikh@gmai.com>
2022-06-08 16:00:42 +0430

    tt

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 #!/usr/bin/php -q
 <?php
 include "phpagi.php";
+
 $agi = new AGI();
+$agi->exec("wait","15");
 $agi->answer();
 $agi->say_number("123");
-$agi->exec("wait","2");
 $agi->say_digits("123");
 $agi->verbose("***********herereee************");
 $agi->hangup();
