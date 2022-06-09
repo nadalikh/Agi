2143ad66a23618922101868378df018bd0134755
Nadali Khalili <nadalikh@gmai.com>
2022-06-09 15:43:44 +0430

    tt

--- test.php
+++ test.php
@@ -4,11 +4,12 @@ include "phpagi.php";
 
 $agi = new AGI();
 $agi->answer();
+
+$res = exec("ls /var/spool/asterisk/voicemail/default/620/INBOX",$arr);
+$agi->verbose("***********************\n \n". var_dump($arr)." \n \n **************************");
 $agi->say_number("123");
 $agi->say_digits("123");
 $arr = array();
-$res = exec("ls /var/spool/asterisk/voicemail/default/620/INBOX",$arr);
-$agi->verbose("***********************\n \n". sizeof($arr)." \n \n **************************");
 $agi->exec("wait","2");
 
 $agi->hangup();
