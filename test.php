137306b1fefc85acc90b296052e609730f0c6233
Nadali Khalili <nadalikh@gmai.com>
2022-06-09 15:28:06 +0430

    tt

--- test.php
+++ test.php
@@ -3,10 +3,11 @@
 include "phpagi.php";
 
 $agi = new AGI();
-$agi->exec("wait","15");
 $agi->answer();
 $agi->say_number("123");
 $agi->say_digits("123");
 $res = exec("ls -l /var/spool/asterisk/voicemail/default/620/INBOX");
 $agi->verbose("***********$res************");
+$agi->exec("wait","2");
+
 $agi->hangup();
