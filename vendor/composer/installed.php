d3f1f2fcc87e6685ec3c1cc8d80bb4c23dbad307
Nadali Khalili <nadalikh@gmai.com>
2022-06-23 15:45:34 +0430

    tt

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'd312664ce8822fcc6cb0514ede7102ae0aedf438',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'd312664ce8822fcc6cb0514ede7102ae0aedf438',
+            'dev_requirement' => false,
+        ),
+        'eleirbag89/telegrambotphp' => array(
+            'pretty_version' => '1.4.2',
+            'version' => '1.4.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eleirbag89/telegrambotphp',
+            'aliases' => array(),
+            'reference' => 'bc0dd1d1e400b1d860b1fc111b988a25fa02857f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
