2d9871fa8883756a4146a7f5818e90296f9d62cf
Nadali Khalili <nadalikh@gmai.com>
2022-06-23 16:04:16 +0430

    tt

--- mysweetHangup.php
+++ mysweetHangup.php
@@ -5,3 +5,4 @@ include "phpagi.php";
 $agi = new AGI();
 //$callerid = $agi->get_variable("CALLERID(name)");
 $agi->verbose("************************\n"."\n***********************************");
+$agi->hangup();
