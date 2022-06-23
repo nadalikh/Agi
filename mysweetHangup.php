d312664ce8822fcc6cb0514ede7102ae0aedf438
Nadali Khalili <nadalikh@gmai.com>
2022-06-23 15:41:38 +0430

    tt

---
+++ mysweetHangup.php
@@ -0,0 +1,7 @@
+#!/usr/bin/php -q
+<?php
+require __DIR__."/vendor/autoload.php";
+include "phpagi.php";
+$agi = new AGI();
+$callerid = $agi->get_variable("CALLERID(name)");
+$agi->verbose("************************\n".$callerid['data']."\n***********************************");
