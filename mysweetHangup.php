d25efc64594bcde5d255231f9bcc59aef715ae8a
Nadali Khalili <nadalikh@gmai.com>
2022-06-24 12:53:17 +0430

    exit

--- mysweetHangup.php
+++ mysweetHangup.php
@@ -7,7 +7,7 @@ $agi = new AGI();
 $ch = curl_init();
 curl_setopt_array(
     $ch, array(
-    CURLOPT_URL => 'https://nkhpro.ir:88/telegram/testcurl.php?chat_id=668578590&message=hey+you',
+    CURLOPT_URL => 'https://nkhpro.ir:88/testcurl.php?chat_id=668578590&message=hey+you',
     CURLOPT_RETURNTRANSFER => true
 ));
 $output = curl_exec($ch);
