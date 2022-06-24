d314a12a2d28e907665cd4ab4078863434fce054
Nadali Khalili <nadalikh@gmai.com>
2022-06-24 12:51:34 +0430

    exit

--- mysweetHangup.php
+++ mysweetHangup.php
@@ -1,12 +1,14 @@
 #!/usr/bin/php -q
 <?php
-require __DIR__."/vendor/autoload.php";
 include "phpagi.php";
-$bot_api_key  = '5362421313:AAGQm8uLoOFlVHk56ZVVAJJmTBKGIJfqYq0';
-$bot_username = 'nkhTestPhp_bot';
-$tel = new Telegram($bot_api_key);
 $agi = new AGI();
 //$callerid = $agi->get_variable("CALLERID(name)");
 //$agi->verbose("************************\n".$callerid['data']."\n***********************************");
-$res = $tel->sendMessage(['chat_id'=>"668578590", 'text'=> "you had call from"]);
-$agi->verbose("************************\n".$res."\n***********************************");
+$ch = curl_init();
+curl_setopt_array(
+    $ch, array(
+    CURLOPT_URL => 'https://nkhpro.ir:88/telegram/testcurl.php?chat_id=668578590&message=hey+you',
+    CURLOPT_RETURNTRANSFER => true
+));
+$output = curl_exec($ch);
+$agi->verbose("************************\n"."\n***********************************");
