a8d22c6002e752032d5ad80cfcc85387cee1fa34
Nadali Khalili <nadalikh@gmai.com>
2022-06-27 19:58:33 +0430

    e

--- mysweetHangup.php
+++ mysweetHangup.php
@@ -13,14 +13,14 @@ $answer = explode(" ",$agi->get_variable("CDR(answer)")['data']);
 $message = "🛑+name+:+$name,";
 $message .= "🛑+caller+id+:+$src,";
 $message .= "🛑+started+at+:+$start[0]+$start[1],";
-$message .= "🛑+duration+:+$duration,";
+$message .= "🛑+duration+:+$duration+s,";
 $message .= "🛑+ended+at+:+$end[0]+$end[1],";
 $message .= "🛑+answered+at+:+$answer[0]+$answer[1],";
-$s = $start[0]." ".$start[1];
+
 $db = new mysqli("localhost", "root","expecto-patronum1379","app");
 if($db->connect_error)
     die("connection faild to db: ". $db->connect_error);
-$db->query("insert into mysweet_cdr (name, callder_id, duration, started, ended, answered) VALUES ('$name', '$src','$duration', '$s', '$end[0] $end[1]', '$answer[0] $answer[1]')");
+$db->query("insert into mysweet_cdr (name, callder_id, duration, started, ended, answered) VALUES ('$name', '$src','$duration', '$start[0] $start[1]', '$end[0] $end[1]', '$answer[0] $answer[1]')");
 
 $ch = curl_init();
 curl_setopt_array(
